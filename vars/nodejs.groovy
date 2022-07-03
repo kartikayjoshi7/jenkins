b26ef601c34a9e6a700c48cd02a008433e1ddcf5
Kartikay <kartikayjoshiprivate@gmail.com>
2022-07-04 04:39:52 +0530

    Cart Jenkins File updated

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,7 +1,30 @@
-def info(message) {
-    echo "INFO: ${message}"
-}
+def call() {
+    pipeline {
+        agent {
+            label 'WORKSTATION'
+        }
 
-def warning(message) {
-    echo "WARNING: ${message}"
+        triggers {
+            pollSCM('*/2 * * * *')
+        }
+
+        stages {
+
+            stage('Compile the code') {
+                steps {
+                    sh 'echo compile code'
+                }
+            }
+            stage('Check the code quality') {
+                steps {
+                    sh 'echo check the code quality'
+                }
+            }
+            stage('Test cases') {
+                steps {
+                    sh 'echo Test cases'
+                }
+            }
+        }
+    }
 }
