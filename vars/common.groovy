d9b55c4bc5110168e48ec97b870ff1bf2199d6ec
Kartikay <kartikayjoshiprivate@gmail.com>
2022-07-10 03:06:43 +0530

    java groovy updated

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,3 +1,3 @@
 def sonarQube() {
-    sh 'sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.6.50 -Dsonar.login=admin -Dsonar.password=admin123'
+    sh 'sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.6.50:9000 -Dsonar.login=admin -Dsonar.password=admin123'
 }
