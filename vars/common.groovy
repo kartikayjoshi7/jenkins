8e795aff8604dcc0e1c19d8615b8b31dc98a5e55
Kartikay <kartikayjoshiprivate@gmail.com>
2022-07-18 02:29:24 +0530

    File updated

--- vars/common.groovy
+++ vars/common.groovy
@@ -60,11 +60,11 @@ def prepareArtifacts() {
         '''
     }
 
-    if(env.PROG_LANG_NAME == "angular"  && env.PROG_LANG_VERSION == "")
+    if(env.PROG_LANG_NAME == "angular"  )
     {
         sh '''
             cd static
-            zip -r ${COMPONENT}-${gitTag}.zip *
+            zip -r ../${COMPONENT}-${gitTag}.zip *
            
         '''
     }
