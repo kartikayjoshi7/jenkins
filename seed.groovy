5965640fc68c73a1c06a08236223a06191557e41
Kartikay <kartikayjoshiprivate@gmail.com>
2022-07-31 01:03:13 +0530

    File updated

--- seed.groovy
+++ seed.groovy
@@ -102,3 +102,25 @@ pipelineJob('Mutable/APP-SETUP') {
         }
     }
 }
+
+
+pipelineJob('Mutable/LAB') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/kartikayjoshi7/terraform-mutable-alb.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
