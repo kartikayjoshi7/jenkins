44b3f64470a6471eea049feae3f4242fd343d282
Kartikay <kartikayjoshiprivate@gmail.com>
2022-08-01 01:48:59 +0530

    File updated

--- seed.groovy
+++ seed.groovy
@@ -124,3 +124,24 @@ pipelineJob('Mutable/ALB') {
         }
     }
 }
+
+pipelineJob('Mutable/DB') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/kartikayjoshi7/terraform-db.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
