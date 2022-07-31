833d1f93b42312ed47a19bf3acad5142983c2c90
Kartikay <kartikayjoshiprivate@gmail.com>
2022-08-01 03:29:58 +0530

    File updated

--- seed.groovy
+++ seed.groovy
@@ -145,3 +145,24 @@ pipelineJob('Mutable/DB') {
         }
     }
 }
+
+pipelineJob('Mutable/All-Infra-Creation') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/kartikayjoshi7/jenkins.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile-Mutable-All-in-one-Infra-Create')
+            'lightweight'(true)
+        }
+    }
+}
