8d75efbbe4f2a4a8863427b23b3b31bcc66bd1b2
Kartikayjoshi7 <kartikayjoshiprivate@gmail.com>
2022-09-28 14:17:56 +0530

    File updated

--- instance.refresh.sh
+++ instance.refresh.sh
@@ -3,3 +3,15 @@
 ASG=$1
 
 aws autoscaling start-instance-refresh --auto-scaling-group-name ${ASG}
+while true; do
+  STATUS=$(aws autoscaling describe-instance-refresh --auto-scaling-group-name ${ASG} | jq .InstanceRefreshes[0].Status | sed -e 's/"//g')
+  echo Instance Refresh Status={$STATUS}
+  if [ "${STATUS}" == "InProgress"  ]; then
+
+    sleep 30
+    elif [ "${STATUS}" == "Successful" ]; then
+        exit 0
+        else
+          exit 1
+    fi
+    done
